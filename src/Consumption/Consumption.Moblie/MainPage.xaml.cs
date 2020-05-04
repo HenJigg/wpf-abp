b1f2c1a4b4eae1d2dcdfe2c061f106987d6a5cff
henji <zhouhaogg789@outlook.com>
2020-05-04 11:25:30 +0800

    Initialize the project structure

---
+++ src/Consumption/Consumption.Moblie/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Consumption.Moblie
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
