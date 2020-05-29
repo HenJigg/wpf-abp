1f744377391bc17e68ee97d51083db74a143c715
zhouhaogg789@outlook.com <zhouhaogg789@outlook.com>
2020-05-29 10:05:37 +0800

    DeskTop: 设计登录页面与ViewModel

--- src/Consumption/Consumption.PC/App.xaml.cs
+++ src/Consumption/Consumption.PC/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Consumption.PC.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,11 @@ namespace Consumption.PC
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LoginView view = new LoginView();
+            view.ShowDialog();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ src/Consumption/Consumption.ViewModel/LoginViewModel.cs
@@ -0,0 +1,106 @@
+﻿/*
+*
+* 文件名    ：LoginViewModel                             
+* 程序说明  : 登录模块
+* 更新时间  : 2020-05-27 15:46
+* 联系作者  : QQ:779149549 
+* 开发者群  : QQ群:874752819
+* 邮件联系  : zhouhaogg789@outlook.com
+* 视频教程  : https://space.bilibili.com/32497462
+* 博客地址  : https://www.cnblogs.com/zh7791/
+* 项目地址  : https://github.com/HenJigg/WPF-Xamarin-Blazor-Examples
+* 项目说明  : 以上所有代码均属开源免费使用,禁止个人行为出售本项目源代码
+*/
+
+namespace Consumption.ViewModel
+{
+    using GalaSoft.MvvmLight;
+    using GalaSoft.MvvmLight.Command;
+    using GalaSoft.MvvmLight.Messaging;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// 登录模块
+    /// </summary>
+    public class LoginViewModel : ViewModelBase
+    {
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+            LogoutCommand = new RelayCommand(LogOut);
+        }
+        #region Property
+        private string userName;
+        private string passWord;
+        private string report;
+        private string isCancel;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; RaisePropertyChanged(); }
+        }
+
+        public string PassWord
+        {
+            get { return passWord; }
+            set { passWord = value; RaisePropertyChanged(); }
+        }
+
+        public string Report
+        {
+            get { return report; }
+            set { report = value; RaisePropertyChanged(); }
+        }
+
+        public string IsCancel
+        {
+            get { return isCancel; }
+            set { isCancel = value; RaisePropertyChanged(); }
+        }
+        #endregion
+
+        #region Command
+
+        public RelayCommand LoginCommand { get; private set; }
+        public RelayCommand LogoutCommand { get; private set; }
+
+        /// <summary>
+        /// 登录系统
+        /// </summary>
+        private void Login()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(UserName) || string.IsNullOrWhiteSpace(PassWord))
+                {
+                    this.Report = "请输入用户名密码";
+                    return;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 登出系统
+        /// </summary>
+        private void LogOut()
+        {
+            Messenger.Default.Send(true, "LogOut");
+        }
+
+        #endregion
+    }
+}
