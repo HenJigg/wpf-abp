15db97f043bc50c0d3a9a96031df4f64d6466b93
zhouhaogg789@outlook.com <zhouhaogg789@outlook.com>
2020-05-29 10:38:15 +0800

    添加项目声明

--- src/Consumption/Consumption.Api/Controllers/BasicController.cs
+++ src/Consumption/Consumption.Api/Controllers/BasicController.cs
@@ -2,10 +2,14 @@
 *
 * 文件名    ：BasicController                          
 * 程序说明  : 基础数据控制器
-* 更新时间  : 2020-05-215 12:03
-* 更新人    : zhouhaogg789@outlook.com
-* 
-*
+* 更新时间  : 2020-05-21 12:03
+* 联系作者  : QQ:779149549 
+* 开发者群  : QQ群:874752819
+* 邮件联系  : zhouhaogg789@outlook.com
+* 视频教程  : https://space.bilibili.com/32497462
+* 博客地址  : https://www.cnblogs.com/zh7791/
+* 项目地址  : https://github.com/HenJigg/WPF-Xamarin-Blazor-Examples
+* 项目说明  : 以上所有代码均属开源免费使用,禁止个人行为出售本项目源代码
 */
 
 namespace Consumption.Api.Controllers
--- src/Consumption/Consumption.Api/Program.cs
+++ src/Consumption/Consumption.Api/Program.cs
@@ -1,20 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Consumption.EFCore.Orm;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Writers;
-using NLog.Web;
+/*
+*
+* 文件名    ：Program     
+* 更新时间  : 2020-05-21 11:44
+* 联系作者  : QQ:779149549 
+* 开发者群  : QQ群:874752819
+* 邮件联系  : zhouhaogg789@outlook.com
+* 视频教程  : https://space.bilibili.com/32497462
+* 博客地址  : https://www.cnblogs.com/zh7791/
+* 项目地址  : https://github.com/HenJigg/WPF-Xamarin-Blazor-Examples
+* 项目说明  : 以上所有代码均属开源免费使用,禁止个人行为出售本项目源代码
+*/
+
 
 namespace Consumption.Api
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Consumption.EFCore.Orm;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.OpenApi.Writers;
+    using NLog.Web;
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
