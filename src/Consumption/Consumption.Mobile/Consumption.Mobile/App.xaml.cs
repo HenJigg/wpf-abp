2dda28879a1b837e4a746de31e289aad86964b2c
henji <zhouhaogg789@outlook.com>
2020-07-12 23:10:40 +0800

    Mobile: Add MaterialDesign by XF.Material, Remove SyncFusion UI

---
+++ src/Consumption/Consumption.Mobile/Consumption.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Android;
+using Android.OS;
+using Foundation;
+using System;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Consumption.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            XF.Material.Forms.Material.Init(this);
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
