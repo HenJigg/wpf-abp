de219055f0bae0ac4acac75c8abe5b389d28cab3
henji <zhouhaogg789@outlook.com>
2020-07-19 21:24:12 +0800

    Add transition animations and related links.

--- src/Consumption/Consumption.PC/MaterialDesignMainWindow.xaml.cs
+++ src/Consumption/Consumption.PC/MaterialDesignMainWindow.xaml.cs
@@ -21,5 +21,20 @@ namespace Consumption.PC
         {
             InitializeComponent();
         }
+
+        private void btnGithub(object sender, RoutedEventArgs e)
+        {
+            Link.OpenInBrowser("https://github.com/HenJigg/WPF-Xamarin-Blazor-Examples");
+        }
+
+        private void btnBilibili(object sender, RoutedEventArgs e)
+        {
+            Link.OpenInBrowser("https://space.bilibili.com/32497462");
+        }
+
+        private void btnQQ(object sender, RoutedEventArgs e)
+        {
+            Link.OpenInBrowser(" http://qm.qq.com/cgi-bin/qm/qr?k=KpcFszjNfY2g-o0q1eEMIoYWbzjSMO2-&authKey=lg1kMENlcHkLO2gejRLvXmGq9Xy6GGb0X1h%2B9QDMhbNxvLLLugAsDQUIuzPJZhDy&group_code=874752819");
+        }
     }
 }
